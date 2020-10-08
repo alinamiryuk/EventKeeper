2e81f08566621d24904f3ebf91c3e8eef7ad88f5
alinamiryuk <alinamiryuk@mail.ru>
2020-10-08 14:51:59 +0300

    mongoose connection

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const express = require('express')
 
 const app = express()
 
+const authRouter = require('./routes/auth')
+
+app.use('/api/auth', authRouter)
+
 module.exports = app
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const http = require('http')
 const app = require('./app')
 const mongoose = require('mongoose')
-const PORT = process.env.PORT || 4000
+const config = require('config')
+
+const PORT = config.get('port') || 4100
 const server = http.createServer(app)
+const DB_KEY = config.get('dbkey')
 
 server.listen(PORT, (err) => {
-  err ? console.log(err) : console.log(`port ${PORT}`)
+  if (err) console.log(err) 
+  console.log(`port ${PORT}`)
+  mongoose.connect(DB_KEY, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
 })
