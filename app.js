438c6d03ca9816ee280184f9bfc451b71f99fba6
alinamiryuk <alinamiryuk@mail.ru>
2020-10-27 00:11:10 +0300

    cross-env

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
-
+const path = require('path')
 const app = express()
 
 const authRouter = require('./routes/auth')
@@ -9,5 +9,12 @@ app.use(morgan('dev'))
 app.use(express.json({ extended: true }))
 app.use(express.urlencoded({ extended: true }))
 app.use('/api/auth', authRouter)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'frontend', 'build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 module.exports = app
